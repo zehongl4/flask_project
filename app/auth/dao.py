bafd99aa7f9a98adf4343d367a5d68e5e23adbeb
zehongl4 <laance2000@outlook.com>
2024-09-08 18:00:16 -0500

    Update

--- app/auth/dao.py
+++ app/auth/dao.py
@@ -5,15 +5,26 @@ Created on Fri Sep  6 19:00:39 2024
 
 @author: xuan
 """
-from app import conn_mysql
+
 import pymysql
+
+# Database connection function
+def conn_mysql():
+    return pymysql.connect(
+        host="127.0.0.1", 
+        port=3306, 
+        user="root", 
+        password="lanzehong0997", 
+        database="flask_db", 
+        charset="utf8"
+    ) 
 
 class UserDAO:
     def __init__(self):
         pass
 
-    # Execute query and return results
-    def query_data(self, sql, params=None):
+    # Execute query and return results              
+    def __query_data(self, sql, params=None):
         conn = conn_mysql()
         try:
             with conn.cursor(pymysql.cursors.DictCursor) as cursor:
@@ -26,7 +37,7 @@ class UserDAO:
             conn.close()
 
     # Insert or update data in database
-    def insert_or_update_data(self, sql, params=None):
+    def __insert_or_update_data(self, sql, params=None):
         conn = conn_mysql()
         try:
             with conn.cursor() as cursor:
@@ -39,14 +50,17 @@ class UserDAO:
             conn.close()
 
     def get_user_password(self, username):
-        return self.query_data("SELECT password FROM user_log WHERE username = %s", (username,))
 
-    def user_exists(self, username):
-        return self.query_data("SELECT password FROM user_log WHERE username = %s", (username,)) is not None
+        return self.__query_data("SELECT password FROM user_log WHERE username = %s", (username,))
 
-    def insert_user(self, username, password):
-        self.insert_or_update_data("INSERT INTO user_log (username, password) VALUES (%s, %s)", (username, password))
+    def check_user_exist(self, username):
+        result = self.__query_data("SELECT password FROM user_log WHERE username = %s", (username,))
+        return result != ()  # Check if the result is not empty
+
+
+    def add_user(self, username, password):
+        self.__insert_or_update_data("INSERT INTO user_log (username, password) VALUES (%s, %s)", (username, password))
 
     def update_password(self, username, password):
-        self.insert_or_update_data("UPDATE user_log SET password = %s WHERE username = %s", (password, username))
+        self.__insert_or_update_data("UPDATE user_log SET password = %s WHERE username = %s", (password, username))
 
